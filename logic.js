ee99cbd1f8bfa8cc1a41b5b222d1953167969b80
Dan Bushong <bushongdan@gmail.com>
2021-01-12 12:00:41 -0600

    api link attempt

--- logic.js
+++ logic.js
@@ -1,1 +1,16 @@
 
+
+    
+$("#search-button").on("click", function() {
+
+ 
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key="+ apiKey;
+    var apiKey = "k82A50CismIhAy2OT09WqCskA56GPHry";
+
+      
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+});
+
