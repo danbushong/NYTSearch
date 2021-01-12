27b109a361c71cdf00bec7ee70b1914f2ed0bca9
Cole <cole.ballard97@gmail.com>
2021-01-12 12:42:28 -0600

    added framework for logic.js

--- logic.js
+++ logic.js
@@ -1,21 +1,43 @@
+function renderArticles(response) {
 
+}
 
-    
-$("#search-button").on("click", function(response) {
+function getSearchParameters() {
 
- 
+}
 
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key="+ apiKey;
-    var apiKey = "k82A50CismIhAy2OT09WqCskA56GPHry";
+function validateInput(searchParameters) {
 
-      
+}
+
+function clearSearchParameters() {
+  
+}
+
+$(document).ready(function() {
+  $("#search-btn").on("click", function(event) {
+    event.preventDefault();
+
+    var searchParameters = getSearchParameters();
+
+    if (!validateInput(searchParameters)) 
+      return;
+
+    const API_KEY = "k82A50CismIhAy2OT09WqCskA56GPHry";
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchParameters.term + "&fq=" + searchParameters.filters + "&api-key=" + API_KEY;
+  
     $.ajax({
-    url: queryURL,
-    method: "GET"
-})
-var searchInput = $("#search-term-input").val();
-var recordsInput = $("#number-records-select").val();
-var startYear = $("#start-year-input").val();
-var endYear = $("#end-year-input").val();
-});
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+  
+        renderArticles(response);
+  
+      });
+  });
 
+  $("clear-btn").on('click', function(event) {
+    event.preventDefault();
+    clearSearchParameters();
+  });
+});
