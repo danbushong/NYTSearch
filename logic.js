1432b275693f5017e5f5c04308cf90c84541f49a
tafseer-khan <tafseer.k.khan@gmail.com>
2021-01-12 13:42:12 -0600

    Merged dan's work

--- logic.js
+++ logic.js
@@ -1,20 +1,28 @@
 function renderArticles(response) {
+  var articleHeadline = response.docs.headline.main;
+  var articleURL = response.docs.web_url;
+
+  console.log(articleURL);
+
+  for (i = 0;  i > $("option").val(i); i++)
+  $("#results").append("<h1>",articleHeadline + "<a>",articleURL);
+
+
+
   var recordsNumber= $("#number-records-select").value();
 }
 
 function getSearchParameters() {
-  var term = $("#search-term-input").value;
-  var startYear= $("#start-year-input").value;
-  var endYear = $("end-year-input").value;
+  var term = $("#search-term-input").value();
+  var term = $("#search-term-input").value();
+  var startYear= $("#start-year-input").value();
+  var endYear = $("end-year-input").value();
+
   
   var searchParameters = {"term": term,"startYear":startYear,"endYear":endYear}
   return searchParameters; 
 
- 
 
-}
-
-function validateInput(searchParameters) {
 
 }
 
@@ -23,18 +31,14 @@ function clearSearchParameters() {
   $("#start-year-input").val("");
   $("end-year-input").val("");
 
-  
 }
 
 $(document).ready(function() {
   $("#search-btn").on("click", function(event) {
     event.preventDefault();
-
     var searchParameters = getSearchParameters();
-
     if (!validateInput(searchParameters)) 
       return;
-
     const API_KEY = "k82A50CismIhAy2OT09WqCskA56GPHry";
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchParameters.term + "&fq=" + searchParameters.filters + "&api-key=" + API_KEY;
   
@@ -47,7 +51,6 @@ $(document).ready(function() {
   
       });
   });
-
   $("clear-btn").on('click', function(event) {
     event.preventDefault();
     clearSearchParameters();
